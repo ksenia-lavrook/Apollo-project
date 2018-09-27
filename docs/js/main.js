7510c2e82f8fb4e99fb8c766d570edb8cb602ef0
Ksenia <ksenia.lavruk@yandex.ru>
2018-09-28 01:12:32 +0300

    last1-commit

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+
+
 	// Скрипт для показа и скрытия выпадающего меню на смартфонах
 	// Создаем переменые для кнопки и для меню
 	var pull = $('#navigation-toggle');
@@ -19,13 +21,43 @@ $(document).ready(function() {
 	    
 	});
 
-	// При изменении размера окна, в большую сторону, если меню было с крыто, показываем его.
+
+
+	// При изменении размера окна, в большую сторону, если меню было скрыто, показываем его
+	// И у кнопки убираем модификатор --active
 	$(window).resize(function(){
 	    var w = $(window).width();
 	    if(w > 992) {
 	        menu.removeAttr('style');
+	        pull.removeClass('navigation__toggle-button--active');
+	    } else {
+
 	    }
 	});
+
+	// Скрываем меню при клике на него на смартфоне и планцете
+	// По клику на ссылку в меню запускаем ф-ю fnstart();
+	$('nav.navigation a').on("click", function(){
+		fnstart();
+	});
+
+	// В ф-ии fnstart(); проверяем - если меню открыто (проверяем по наличию класса --active у кнопки pull)
+	// тогда убираем класс модификатор --active у кнопки pull
+	// и сворачиваем/скрываем меню 
+	function fnstart(){	
+		if ( $("#navigation-toggle").hasClass("navigation__toggle-button--active")  ) {
+   			pull.toggleClass('navigation__toggle-button--active');
+			menu.slideToggle();
+		}
+	};
+
+
+
+
+	
+
+
+
 
 
 	// Вызов слайдера owl-carousel
@@ -34,11 +66,42 @@ $(document).ready(function() {
 		navigation: true,
 		theme: "top-slider-theme",
 		navigationText : ["",""],
-		slideSpeed: 800
+		slideSpeed: 600
 	});
 
+
+
+	//slide2id - плавная прокрутка по ссылкам внутри страницы
+	$("nav a,a[href='#top'],a[rel='m_PageScroll2id'],a.PageScroll2id").mPageScroll2id({
+	    highlightSelector:"nav a"
+	});
 	
+
+
+//Анимация
+
+$('.logo__name').hover(function () {
+  $(this).addClass('magictime spaceInUp');
 });
+
+setInterval(function(){ 
+	$('.logo__name').toggleClass('magictime spaceInUp');
+}, 3000 );
+
+$('.logo__slogan').hover(function () {
+  $(this).addClass('magictime tinDownIn');
+});
+
+setInterval(function(){ 
+	$('.logo__slogan').toggleClass('magictime tinDownIn');
+}, 3000 );
+
+$('.service-description__img').hover(function () {
+  $(this).addClass('magictime slideLeftReturn');
+});
+
+});
+
 
 
 
